58b61231df8830ebbcf41cafd0c74268792176f2
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-02-25 10:06:04 -0500

    updated

---
+++ config.py
@@ -0,0 +1,18 @@
+1 # Pygame Configuration Module
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (147,112,219)
+PINK = (255,192,203)
+HOT_PINK = (255,0,144)
+
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+FPS = 60
+
+TITLE = "Pygame Window"
--- main1.py
+++ main1.py
@@ -1,34 +1,55 @@
-# Rectangle
+import pygame
+import sys
+import config
+
+
+pygame.init()
+screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
+pygame.display.set_caption(config.TITLE)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(config.HOT_PINK) 
+
+
+def init_game():
+    return pygame.display.set_mode((800,600))
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False 
+        return True 
+
+
 def draw_rectangle(screen, rect, color, thickness):
-    """Draws a rectangle on the Pygame window."""
+
     pygame.draw.rect(screen, color, rect, thickness)
 
-# Call the rectangle function from your main ( ) function like so:
-# In the list my_rect1, 100 and 100 are the x,y coordinates for the top left corner of the rectangle
-# Rectangle has a width of 200 pixels
-# Height of rectangle is 150 pixels
+
 my_rect1 = [100, 100, 200, 150]
-thickness1 = 8 # Line thickness (width) in pixels
+thickness1 = 8 
 
 draw_rectangle(screen, my_rect1, config.GREEN, thickness1)
 
-# Write this function above your main( ) function
+
 def draw_circle(screen, center, radius, color, thickness):
-    """Draws a circle on the Pygame window."""
+    
     pygame.draw.circle(screen, color, center, radius, thickness)
 
-# Define circle arguments for the function call
-circle_center = (300, 200)  # Center point of the circle (x, y)
-circle_radius = 50           # Radius of the circle
-circle_color = config.GREEN   # Color of the circle
-circle_thickness = 0         # 0 pixels creates a filled in circle
+circle_center = (300, 200)  
+circle_radius = 50           
+circle_color = config.GREEN   
+circle_thickness = 0         
 
-# Call the function that draws the circle from your main() function
 draw_circle(screen, circle_center, circle_radius, circle_color, circle_thickness)
 
-# Write function above your main( ) function
 def draw_line(surface, color, start_pos, end_pos, thickness):
-    """Draws a line on the screen with the specified color and thickness."""
+    
 
     pygame.draw.line(surface, color, start_pos, end_pos, thickness)
 
